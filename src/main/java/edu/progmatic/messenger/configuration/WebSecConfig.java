2c0bc5ba71f64bede1465b8ea0a38f6dbb5d8c48
Toth Csaba <toth.csaba.szeged@gmail.com>
2020-06-22 10:59:56 +0200

    login adding

---
+++ src/main/java/edu/progmatic/messenger/configuration/WebSecConfig.java
@@ -0,0 +1,27 @@
+package edu.progmatic.messenger.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").
+                password("password").roles("USER").build());
+        return manager;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
