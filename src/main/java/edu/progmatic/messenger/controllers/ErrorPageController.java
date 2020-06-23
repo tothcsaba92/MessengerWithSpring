9446e1b15d251c1dac469557cbbb9d9a671f8594
Botos János <botosjano@gmail.com>
2020-06-23 23:04:07 +0200

    error page added

---
+++ src/main/java/edu/progmatic/messenger/controllers/ErrorPageController.java
@@ -0,0 +1,44 @@
+package edu.progmatic.messenger.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+public class ErrorPageController implements ErrorController {
+    private static final String ERR_PATH = "/error";
+
+    private ErrorAttributes errorAttributes;
+
+    @Autowired
+    public ErrorPageController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+
+    @GetMapping(ERR_PATH)
+    public String error(Model model, HttpServletRequest request) {
+        ServletWebRequest requestAttributes = new ServletWebRequest(request);
+        Map<String, Object> error = this.errorAttributes.getErrorAttributes(requestAttributes, true);
+
+        model.addAttribute("timestamp", error.get("timestamp"));
+        model.addAttribute("error", error.get("error"));
+        model.addAttribute("message", error.get("message"));
+        model.addAttribute("path", error.get("path"));
+        model.addAttribute("status", error.get("status"));
+
+        return "detailedError";
+    }
+
+
+    @Override
+    public String getErrorPath() {
+        return ERR_PATH;
+    }
+}
